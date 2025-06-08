fc9b13bb858bf62cfd5ea16ab3b1295a419aab94
FaveGrace <onyemafavour1994@gmail.com>
2025-06-08 11:46:34 +0100

    Updates

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -14,11 +14,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
-    },
     isVerified: {
         type: Boolean,
         default: false
